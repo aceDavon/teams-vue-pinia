0458be0149e29a6f3822bf67d9273f25de533f51
aceDavon <makojidavid@gmail.com>
2022-12-25 23:23:59 +0100

    Implement the Modal component
    Implement the modal styling, transitions and teleporting to fix common
    styling issues

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -1,23 +1,48 @@
 <script setup>
 import Team from "@/components/Team.vue";
+import { ref } from "vue";
 import { useStore } from "../store/Sore";
+import Modal from "./Modal.vue";
 
 const TeamStore = useStore();
 
 TeamStore.fill();
+
+const modalShow = ref(false);
 </script>
 
 <template>
   <div class="flex flex-col gap-2 px-2 py-6">
-    <div class="flex justify-between items-center">
+    <div class="lg:flex justify-between items-center">
       <button
         :disabled="TeamStore.members.length == TeamStore.limit"
         class="bg-blue-400 text-white text-sm capitalze p-2 rounded-md"
+        @click="modalShow = true"
       >
         Add Member ({{ TeamStore.limit - TeamStore.members.length }}) Slots
         remaining
       </button>
-      <span class="text-xl font-bold text-green-500">{{ TeamStore.name }}</span>
+      <p class="text-xl font-bold text-green-500 text-center mt-4 lg:mt-0 lg:border-0 border-b border-b-3 border-b-green-500 lg:text-right">{{ TeamStore.name }}</p>
+      <Modal :show="modalShow">
+        <template #header>Add new Members to Team</template>
+        <template #default>
+          <form action="#">
+            <div class="flex gap-2 my-2">
+              <input
+                type="text"
+                class="flex-1 px-2 py-1 rounded"
+                placeholder="Email address..."
+              />
+              <button>Add</button>
+            </div>
+          </form>
+        </template>
+        <template #footer>
+          <button class="bg-gray-600 px-2 py-1 text-white rounded-md" @click="modalShow = false">
+            close
+          </button>
+        </template>
+      </Modal>
     </div>
     <Team />
     <span class="italic text-right capitalize">
