5c80d67c5e908ea099c12603f5c0d90f15a517f7
aceDavon <makojidavid@gmail.com>
2022-12-25 23:21:39 +0100

    Add a modal component
    Extract a common moda component to the components directory

---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  show: Boolean,
+});
+</script>
+
+<template>
+  <Teleport to="body">
+    <Transition
+      enter-from-class="opacity-0"
+      enter-active-class="transition delay-500 scale-125"
+      leave-active-class="transition delay-300 scale-125"
+      leave-to-class="opacity-0"
+    >
+      <div class="fixed inset-0 grid place-items-center modal-mask" v-if="show">
+        <div
+          class="bg-white rounded w-8/12 max-w-md p-2 text-left space-y-4 text-gray-600"
+        >
+          <slot name="header"></slot>
+          <slot></slot>
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<style>
+.modal-mask {
+  background-color: rgba(0, 0, 0, 0.65);
+  z-index: 999;
+}
+</style>
